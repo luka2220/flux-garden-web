fdbc62243561eaf5dd63128025aa2fcaff50a709
Luka Piplica <piplicaluka64@gmail.com>
2025-10-10 18:39:28 -0400

    application init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// Root vue component
+
+import Feed from "./components/Feed.vue";
+</script>
+
+<template>
+  <h1>Flux Garden Web Client</h1>
+
+  <Feed msg="Flux garden rss feed reader 🪴" />
+</template>
---
+++ src/components/Feed.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h3>{{ msg }}</h3>
+  <h3>RSS Feed</h3>
+
+  <div>
+    <a href="https://feeds.feedburner.com/TechCrunch/">Tech Crunch</a>
+  </div>
+</template>
